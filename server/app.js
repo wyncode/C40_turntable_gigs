ac00a178178ce417b75d6f75a5546a1af98e8275
whoisdesirtech <jean@whoisdesir.com>
2020-12-13 16:29:29 -0500

    adding routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,29 +1,50 @@
 require('./db/config');
 const express = require('express'),
+  morgan = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  openRoutes = require('./routes/open'),
+  userRouter = require('./routes/secure/users'),
+  taskRouter = require('./routes/secure/tasks'),
+  passport = require('./middleware/authentication'),
   path = require('path'),
-  morgan = require('morgan');
-openRoutes = require('./routes/open');
+  fileUpload = require('express-fileupload'),
+  app = express();
 
-const app = express();
-
-//Middleware
+//Parse incoming JSON into objects
 app.use(express.json());
+// Log all requests
 app.use(morgan('dev'));
 
 // Unauthenticated routes
-app.use(openRoutes);
+app.use('/api/users', openRoutes);
 
-// Serve any static files
+app.use(cookieParser());
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
 }
 
-// Any authentication middleware and related routing would be here.
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/images'
+  })
+);
+//  Authentication Middleware
+app.use('/api/*', passport.authenticate('jwt', { session: false }));
 
-// Handle React routing, return all requests to React app
+// Authenticated Routes
+app.use('/api/users', userRouter);
+app.use('/api/tasks', taskRouter);
+
 if (process.env.NODE_ENV === 'production') {
+  // Handle React routing, return all requests to React app
   app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    response.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
   });
 }
+
 module.exports = app;
