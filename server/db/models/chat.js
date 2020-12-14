6f0365238f4feff01613b04309a12dad3b44797d
JadivaM <jmont261@fiu.edu>
2020-12-14 16:10:11 -0500

    added receiver to the chat model and changed type to object id and fixed UserSchema to usreSchema in several places

--- server/db/models/chat.js
+++ server/db/models/chat.js
@@ -1,25 +1,29 @@
 const Schema = mongoose.Schema;
 
 const ChatSchema = new Schema(
-    {
-        messages: [
-            { 
-                type: String, 
-                required: true 
-            }
-        ],
-        sender: { 
-            type: String, 
-            required: true 
-        },
-        read: { 
-            type: Date 
-        },
+  {
+    messages: [
+      {
+        type: String,
+        required: true
+      }
+    ],
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     },
-    { 
-        timestamps: true 
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    read: {
+      type: Date
     }
+  },
+  {
+    timestamps: true
+  }
 );
 
-const Chat = mongoose.model("Chat", ChatSchema);
+const Chat = mongoose.model('Chat', ChatSchema);
 module.exports = { Chat };
