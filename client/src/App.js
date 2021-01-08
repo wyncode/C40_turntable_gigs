c614f4886ea2c093ee2925c7094cd4714e1c1dfb
whoisdesirtech <jean@whoisdesir.com>
2021-01-08 02:13:16 -0500

    added the about page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import FindGigs from './pages/FindGigs';
 import FindDjs from './pages/FindDjs';
 import Profile from './pages/Profile';
 import Gigs from './pages/Gigs';
+import About from './pages/About';
 import { AppContextProvider } from './context/AppContext';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/search/djs" component={FindDjs} />
           <Route exact path="/profile/:id" component={Profile} />
           <Route exact path="/gigs" component={Gigs} />
+          <Route exact path="/about" component={About} />
         </Switch>
       </BrowserRouter>
     </AppContextProvider>
